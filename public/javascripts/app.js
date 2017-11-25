7033e67235525f03c520515c974cfdcfcfc8a377
Drew André <dandre@conncoll.edu>
2017-11-24 23:24:51 -0500

    switch out click touchstart for touchstart;

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $('#main-nav > ul').hide();
 
-  $(document).on('click touchstart', function(){
+  $(document).on('touchstart', function(){
     $('#main-nav > ul').slideUp();
     $('.below-nav').removeClass( "canvas-blur" )
   });
 
-  $('#main-nav').on('click touchstart', function() {
+  $('#main-nav').on('touchstart', function() {
     event.stopPropagation();
     $('.below-nav').toggleClass( "canvas-blur" )
     $('#main-nav > ul').slideToggle('medium');
