4edc338713e7383ca356dd5d141dea4a50bfb081
Drew André <dandre@conncoll.edu>
2017-11-25 00:25:07 -0500

    add user-select for menu > ul > ul

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,19 +2,14 @@ $(document).ready(function() {
   $('#main-nav > ul').hide();
 
   $('.below-nav').on('click', function(){
-    $('#main-nav > ul').slideUp();
+    $('#main-nav > ul').slideUp('fast');
     $('.below-nav').removeClass( "canvas-blur" )
   });
 
   $('#menu').on('click', function() {
     event.stopPropagation();
     $('.below-nav').toggleClass("canvas-blur" )
-    $('#main-nav > ul').slideToggle('medium');
+    $('#main-nav > ul').slideToggle('fast');
   });
-
-  // $('#main-nav > ul').on('click', function() {
-  //   // event.stopPropagation();
-  //   $('#main-nav > ul > ul').slideToggle('medium');
-  // });
 
 });
