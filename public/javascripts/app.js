2652a825b46eba31b8828e062a79ffde5900bcf4
Drew André <dandre@conncoll.edu>
2017-11-27 21:43:47 -0500

    prep for new branch

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
   $('#main-nav > ul').hide();
+});
 
-  $('.below-nav').on('click', function(){
-    event.stopPropagation();
-    $('#main-nav > ul').slideUp('fast');
-    $('.below-nav').removeClass( "canvas-blur" )
-  });
+$('.below-nav').on('click', function(e){
+  e.stopPropagation();
+  $('#main-nav > ul').slideUp('fast');
+  $('.below-nav').removeClass( "canvas-blur" )
+});
 
-  $('#menu').on('click', function() {
-    event.stopPropagation();
-    $('.below-nav').toggleClass("canvas-blur" )
-    $('#main-nav > ul').slideToggle('fast');
-  });
+$('#menu').on('click', function(e) {
+  e.stopPropagation();
+  $('.below-nav').toggleClass("canvas-blur" )
+  $('#main-nav > ul').slideToggle('fast');
+});
 
-  $('#main-nav ul li').on('click', function() {
-    event.stopPropagation();
-    $('#main-nav ul li ul'[0]).slideDown('fast');
-  });
+$('#main-nav ul li').on('click', function(e) {
+  e.stopPropagation();
+  $('#main-nav ul li ul'[0]).slideDown('fast');
 });
