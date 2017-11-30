b8508c275a014bf7732aa1e513ef50d01cf297fd
Drew André <dandre@conncoll.edu>
2017-11-29 20:17:38 -0500

    add show page photos

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,7 +2,7 @@ $(document).on('touchmove', function(e) {
   e.preventDefault();
 });
 
-$('html, body').scrollTop(1);
+// $('html, body').scrollTop(1);
 
 $(document).ready(function() {
   $('#main-nav > ul').hide();
@@ -16,7 +16,6 @@ $('.below-nav').on('click', function(e){
 
 $('#menu').on('click', function(e) {
   e.stopPropagation();
-  $('#notice').toggle();
   $('.below-nav').toggleClass("canvas-blur" )
   $('#main-nav > ul').slideToggle('fast');
 });
@@ -25,3 +24,16 @@ $('#main-nav ul li').on('click', function(e) {
   e.stopPropagation();
   $('#main-nav ul li ul'[0]).slideDown('fast');
 });
+
+// if(!$('#main-nav > ul').is(':visible')){
+  $('.work-image').each( function() {
+    $(this).on('mouseenter', function(e) {
+      e.stopPropagation();
+      $(this).children("div").fadeIn('fast');
+    });
+    $(this).on('mouseleave', function(e) {
+      e.stopPropagation();
+      $(this).children("div").fadeOut('fast');
+    });
+  });
+// }
