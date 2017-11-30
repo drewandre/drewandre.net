2cc230fb8307c0e668396511661ee6f3c264d179
Drew André <dandre@conncoll.edu>
2017-11-30 13:05:46 -0500

    add detail to show pages

--- app.rb
+++ app.rb
@@ -3,27 +3,7 @@ require 'sinatra/activerecord'
 require 'sinatra/reloader'
 require 'sinatra/flash'
 require 'sass/plugin/rack'
-
-helpers do
-  def img(category, name)
-    title = File.basename(name,File.extname(name)).gsub(/[_-]/, ' ').split.map(&:capitalize).join(' ')
-    return (
-      "<div class='work-image'>
-        <img class='work-tile' src='/images/#{category}/#{name}' alt='#{title}'/>
-        <div id='work-text'><h4>#{title}</h4></div>
-      </div>"
-    )
-  end
-
-  def gather_images_from(folder)
-    images = []
-    Dir.foreach("public/images/#{folder}") do |file|
-      next if file == '.' or file == '..'
-      images.push(img(folder, file))
-    end
-    return images
-  end
-end
+require_relative 'app/helpers/helpers.rb'
 
 enable :sessions
 
@@ -68,50 +48,63 @@ end
 get '/work/web' do
     @title = 'WORK';
     @subtitle = '(WEB)';
-    @photos = gather_images_from('web')
+    @photos = gather_top_images_from('web')
   erb :work
 end
 
 get '/work/lighting' do
   @title = 'WORK';
   @subtitle = '(LIGHTING)';
-  @photos = gather_images_from('lighting')
+  @photos = gather_top_images_from('lighting')
   erb :work
 end
 
 get '/work/web/palette' do
   @title = 'Palette';
-  @photo = 'palette_photo_here'
+  @subtitle = 'Smart LED controller';
+  @photos = gather_all_images_from('web/palette')
   erb :work
 end
 
 get '/work/web/reporev' do
   @title = 'RepoRev';
-  @photo = 'reporev_photo_here'
+  @subtitle = 'GitHub Repo Search';
+  @photos = gather_all_images_from('web/reporev')
+  erb :work
+end
+
+get '/work/web/portfolio' do
+  @title = 'drew-andre.com';
+  @subtitle = 'Portfolio';
+  @photos = gather_all_images_from('web/portfolio')
   erb :work
 end
 
 get '/work/lighting/aura' do
   @title = 'Aura';
-  @photo = 'palette_photo_here'
+  @subtitle = 'Smart LED controller';
+  @photos = gather_all_images_from('lighting/aura')
   erb :work
 end
 
 get '/work/lighting/boston' do
   @title = 'Boston';
-  @photo = 'palette_photo_here'
+  @subtitle = 'LED Drum Kit';
+  @photos = gather_all_images_from('lighting/boston')
   erb :work
 end
 
-get '/work/lighting/tremont-street' do
+get '/work/lighting/tremont' do
   @title = 'Tremont Street';
-  @photo = 'palette_photo_here'
+  @subtitle = 'Reactive suite lighting';
+  @photos = gather_all_images_from('lighting/tremont_street')
   erb :work
 end
 
 get '/work/lighting/winchester' do
   @title = 'Winchester';
-  @photo = 'palette_photo_here'
+  @subtitle = 'Reactive dorm room lighting';
+  @photos = gather_all_images_from('lighting/winchester')
   erb :work
 end
 
--- public/javascripts/helpers/image_helper.js
+++
@@ -1,5 +0,0 @@
-helpers do
-  def img(category, name)
-    `<img src='images/${category}/${name}' alt='${name}' />`
-  end
-end
