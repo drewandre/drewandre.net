c36eff8e406134f67a331299cd71b5a400f142a9
Drew André <dandre@conncoll.edu>
2017-11-30 13:05:46 -0500

    add detail to show pages

---
+++ app/helpers/helpers.rb
@@ -0,0 +1,59 @@
+require 'yaml'
+
+helpers do
+  def img(folder, file)
+    title = File.basename(file,File.extname(file)).gsub(/[_-]/, ' ').split.map(&:capitalize).join(' ')
+    return (
+      "<div class='work-link'>
+        <img class='work-tile' src='/projects/#{folder}/#{file}' alt='#{title}'/>
+      </div>"
+    )
+  end
+
+  def gather_all_images_from(folder)
+    images = []
+    Dir.foreach("public/projects/#{folder}") do |file|
+      next if file == '.' or file == '..'
+      description = YAML::load_file "public/projects/#{folder}/description.yml"
+      case File.extname(file)
+      when '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'
+        images.push(img(folder, file))
+      end
+    end
+    return images
+  end
+
+  def top_img(folder, project, file, description)
+    title = File.basename(file,File.extname(file)).gsub(/[_-]/, ' ').split.map(&:capitalize).join(' ')
+    return (
+      "<a class='work-link' href='#{folder}/#{project}'>
+        <div class='work-image'>
+          <img class='work-tile' src='/projects/#{folder}/#{project}/#{file}' alt='#{title}'/>
+          <div id='work-text'>
+            <h4>#{description['title']}</h4>
+            <h5>#{description['description']}</h5>
+          </div>
+        </div>
+      </a>"
+    )
+  end
+
+  def gather_top_images_from(folder)
+    images = []
+    Dir.foreach("public/projects/#{folder}") do |project|
+      next if project == '.' or project == '..'
+      Dir.foreach("public/projects/#{folder}/#{project}") do |file|
+        next if file == '.' or file == '..'
+        description = YAML::load_file "public/projects/#{folder}/#{project}/description.yml"
+        case File.extname(file)
+        when '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'
+          if File.basename(file, File.extname(file)).start_with?('top')
+            images.push(top_img(folder, project, file, description))
+          end
+        end
+      end
+    end
+    return images
+  end
+
+end
