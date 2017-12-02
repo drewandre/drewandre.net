e5ad41d2a9e17a6788fe232ed76f0a2d371b00c9
Drew André <dandre@conncoll.edu>
2017-12-02 18:13:23 -0500

    clean up jquery

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,39 +1,38 @@
-$(document).ready(function() {
+$(function() {
   $('#main-nav > ul').hide();
   $('.work-text').hide();
-});
 
-$('.below-nav').on('click', function(e){
-  e.stopPropagation();
-  $('#main-nav > ul').slideUp('fast');
-  $('.below-nav').removeClass( "canvas-blur" )
-});
-
-$('#main-nav').on('mouseenter mouseleave', function(e) {
-  e.stopPropagation();
-  $('.below-nav').toggleClass("canvas-blur" )
-  $('#main-nav > ul').slideToggle('fast');
-});
-
-$('#main-nav ul li').on('click', function(e) {
-  e.stopPropagation();
-  $('#main-nav ul li ul'[0]).slideDown('fast');
-});
-
-$('.work-link').each( function() {
-  $(this).on('mouseenter', function(e) {
+  $('.below-nav').on('click', function(e){
     e.stopPropagation();
-    $(this).children("div").fadeIn('fast');
-    $(this).children(".work-tile").toggleClass("image-darken");
+    $('#main-nav > ul').slideUp('fast');
+    $('.below-nav').removeClass( "canvas-blur" )
   });
 
-  $(this).on('mouseleave', function(e) {
+  $('#main-nav').on('mouseenter mouseleave', function(e) {
     e.stopPropagation();
-    $(this).children("div").fadeOut('fast');
-    $(this).children(".work-tile").toggleClass("image-darken");
+    $('.below-nav').toggleClass("canvas-blur" )
+    $('#main-nav > ul').slideToggle('fast');
   });
-});
 
-// to allow :active styles to work in your CSS on a page in Mobile Safari
-document.addEventListener("touchstart", function(){}, true);
-// $('html, body').scrollTop(1);
+  $('#main-nav ul li').on('click', function(e) {
+    e.stopPropagation();
+    $('#main-nav ul li ul'[0]).slideDown('fast');
+  });
+
+  $('.work-link').each( function() {
+    $(this).on('mouseenter', function(e) {
+      e.stopPropagation();
+      $(this).children("div").fadeIn('fast');
+      $(this).children(".work-tile").toggleClass("image-darken");
+    });
+
+    $(this).on('mouseleave', function(e) {
+      e.stopPropagation();
+      $(this).children("div").fadeOut('fast');
+      $(this).children(".work-tile").toggleClass("image-darken");
+    });
+  });
+
+  // to allow :active styles to work on Mobile Safari
+  document.addEventListener("touchstart", function(){}, true);
+});
